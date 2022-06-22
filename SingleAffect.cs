c0ef323d102fd1a96e652c837ab6248e33c1c04f
TheRoaringPigeon <107893565+TheRoaringPigeon@users.noreply.github.com>
2022-06-21 21:52:11 -0400

    Create SingleAffect.cs

---
+++ SingleAffect.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SingleAffect : MonoBehaviour
+{
+    public void Recycle(){
+        gameObject.SetActive(false);
+    }
+}
